320c0c2b892ccc99b11d58b64ce50b2a6453e86e
azamated <azamated@gmail.com>
2020-04-13 16:47:12 -0500

    11.1 completed

--- pyneng-examples-exercises-master/exercises/11_modules/task_11_1.py
+++ pyneng-examples-exercises-master/exercises/11_modules/task_11_1.py
@@ -30,14 +30,45 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 
 def parse_cdp_neighbors(command_output):
+	output = command_output.split('\n')
 	final_dict = {}
-	local_device = ()
-	
+	for line in output:
+		if line.startswith('SW'):
+			line_parse = line.split('>')
+			deviceid = (line_parse[0])        #device as first element of tuple
+		elif line.startswith('R'):
+			temp_list_keys = []
+			temp_list_keys.append(deviceid)
+			temp_list_values = []
+			
+			line_parse = line.split()
+			intf = line_parse[1] + line_parse[2]    	  #local interfaces for the device
+			neighbour = line_parse[0] 					  #neighbours for the device
+			portid = line_parse[-2] + line_parse[-1]      #neighbours' ports
+			
+			temp_list_keys.append(intf)               
+			keys = tuple(temp_list_keys) 				  #dict key - tuple
+			temp_list_values.append(neighbour) 				
+			temp_list_values.append(portid)               
+			values = tuple(temp_list_values)  			   #dict value - tuple
+			
+			final_dict[keys] = values
+	return final_dict
+
+
+with open('sh_cdp_n_sw1.txt') as file:
+	show = file.read()
+print (parse_cdp_neighbors(show))
+
+
+
+"""def parse_cdp_neighbors(command_output):
+	final_dict = {}
 	with open(command_output) as f:
 		for line in f:
 			if line.startswith('SW'):
 				line_parse = line.strip().split('>')
-				device = (line_parse[0])    #device as first element of tuple
+				device = (line_parse[0])   					 #device as first element of tuple
 			elif line.startswith('R'):
 				temp_list_keys = []
 				temp_list_keys.append(device)
@@ -58,5 +89,5 @@ def parse_cdp_neighbors(command_output):
 		return final_dict
 
 				
-print (parse_cdp_neighbors('sh_cdp_n_sw1.txt'))
+print (parse_cdp_neighbors('sh_cdp_n_sw1.txt')) """
 
