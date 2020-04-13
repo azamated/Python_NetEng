3a37354883ed008d0c3beac8a7dfa9a1063b3ce9
azamated <azamated@gmail.com>
2020-04-13 15:12:32 -0500

    11.1 completed

--- pyneng-examples-exercises-master/exercises/11_modules/task_11_1.py
+++ pyneng-examples-exercises-master/exercises/11_modules/task_11_1.py
@@ -28,3 +28,35 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+
+def parse_cdp_neighbors(command_output):
+	final_dict = {}
+	local_device = ()
+	
+	with open(command_output) as f:
+		for line in f:
+			if line.startswith('SW'):
+				line_parse = line.strip().split('>')
+				device = (line_parse[0])    #device as first element of tuple
+			elif line.startswith('R'):
+				temp_list_keys = []
+				temp_list_keys.append(device)
+				temp_list_values = []
+				
+				line_parse = line.strip().split()
+				intf = line_parse[1] + line_parse[2]    	  #local interfaces for the device
+				neighbour = line_parse[0] 					  #neighbours for the device
+				portid = line_parse[-2] + line_parse[-1]      #neighbours' ports
+				
+				temp_list_keys.append(intf)               
+				keys = tuple(temp_list_keys) 				  #dict key - tuple
+				temp_list_values.append(neighbour) 				
+				temp_list_values.append(portid)               
+				values = tuple(temp_list_values)  			   #dict value - tuple
+				
+				final_dict[keys] = values
+		return final_dict
+
+				
+print (parse_cdp_neighbors('sh_cdp_n_sw1.txt'))
+
