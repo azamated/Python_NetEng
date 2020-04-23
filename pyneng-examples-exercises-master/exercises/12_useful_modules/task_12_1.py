60640a2a8f379fab5413d6c72a84525484430dda
azamated <azamated@gmail.com>
2020-04-23 15:05:08 -0500

    12.1 completed

--- pyneng-examples-exercises-master/exercises/12_useful_modules/task_12_1.py
+++ pyneng-examples-exercises-master/exercises/12_useful_modules/task_12_1.py
@@ -14,4 +14,20 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+import subprocess
 
+
+def ping_ip_addresses(ip_addresses):
+	tuple_ip = ()
+	ok_ip = []
+	ko_ip = []
+	for item in ip_addresses:
+		result = subprocess.run(["ping", "-c", "3", "-n", item], stdout=subprocess.DEVNULL)
+		if result.returncode == 0:
+			ok_ip.append(item)
+		else:
+			ko_ip.append(item)
+	tuple_ip = (ok_ip, ko_ip)
+	return tuple_ip
+
+print (ping_ip_addresses(['8.8.8.8', '1.1.1.1', '192.168.5.6', '4.4.4.4', '10.5.6.97']))
